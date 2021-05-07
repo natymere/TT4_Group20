48923363af4cf897a48c129596d0abd40d9d39be
Jem Tan <jeremytandev@gmail.com>
2021-05-07 11:07:44 +0800

    added components based on figma sketch

---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TransactionHistory() {
+  return (
+    <div>
+      <h1>TransactionHistory page</h1>
+    </div>
+  )
+}
---
+++ src/components/TransactionPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TransactionPage() {
+  return (
+    <div>
+      <h1>transaction page</h1>
+    </div>
+  )
+}
